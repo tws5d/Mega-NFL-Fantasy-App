36946f00cc248ff40cc25b65f6dbfc4d44e80e97
tws5d <tylersnead85@gmail.com>
2025-06-07 06:06:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from PIL import Image
 image = Image.open("Banner.jpg")
 st.image(image, use_container_width=True)
 
-position = st.selectbox(
-    "Choose a position:",
-    options=["QB", "RB", "WR", "TE", "FLEX", "DEF"]
-)
+col1, col2 = st.columns([1, 5])  # adjust ratios as needed
+
+with col1:
+    position = st.selectbox(
+        " ",
+        options=["QB", "RB", "WR", "TE", "FLEX", "DEF"]
+    )
+
 
