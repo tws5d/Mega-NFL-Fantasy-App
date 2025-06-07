229b6a2bbb2cd0a7f925a1a60789f8bd65afeea4
tws5d <tylersnead85@gmail.com>
2025-06-07 11:17:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ if position == "DEF" and player:
     col1_stats, col2_stats = st.columns([1, 5])
 
     with col2_stats:
-        st.markdown("<div style='margin-top: 25px;'></div>", unsafe_allow_html=True)
+        st.markdown("<div style='margin-top: 20px;'></div>", unsafe_allow_html=True)
         stat_col1, stat_col2 = st.columns(2)
 
         with stat_col1:
