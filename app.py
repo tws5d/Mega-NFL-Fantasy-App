e016a8b30e3b237bcf87e958c958684a97606113
tws5d <tylersnead85@gmail.com>
2025-06-07 06:10:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+qb_list = ["Patrick Mahomes", "Josh Allen", "Jalen Hurts"]
+rb_list = ["Christian McCaffrey", "Austin Ekeler", "Derrick Henry"]
+wr_list = ["Justin Jefferson", "Tyreek Hill", "Ja'Marr Chase"]
+te_list = ["Travis Kelce", "George Kittle", "Mark Andrews"]
+def_list = ["49ers", "Eagles", "Cowboys"]
+
 col1, col2 = st.columns([1, 5])  # adjust ratios as needed
 
 with col1:
@@ -21,4 +27,17 @@ with col1:
         options=["QB", "RB", "WR", "TE", "FLEX", "DEF"]
     )
 
+with col2:
+    if position == "QB":
+        player = st.selectbox(" ", qb_list)
+    elif position == "RB":
+        player = st.selectbox(" ", rb_list)
+    elif position == "WR":
+        player = st.selectbox(" ", wr_list)
+    elif position == "TE":
+        player = st.selectbox(" ", te_list)
+    elif position == "FLEX":
+        player = st.selectbox(" ", rb_list + wr_list + te_list)
+    elif position == "DEF":
+        player = st.selectbox(" ", def_list)
 
