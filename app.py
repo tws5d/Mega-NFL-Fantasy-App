389e8c9bccc709a4cce1d0aa0a92d7ef9a743925
tws5d <tylersnead85@gmail.com>
2025-06-07 14:17:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,14 +28,14 @@ def get_implied_points(team_full_name, opponent_full_name, is_home_team):
 
     data = response.json()
     with open("odds_log.txt", "w") as f:
-    for game in data:
-        f.write(f"{game['home_team']} vs {game['away_team']}\n")
-        for b in game["bookmakers"]:
-            if b["key"] == "draftkings":
-                for m in b["markets"]:
-                    f.write(f"  Market: {m['key']}\n")
-                    for o in m["outcomes"]:
-                        f.write(f"    {o['name']}: {o['point']}\n")
+        for game in data:
+            f.write(f"{game['home_team']} vs {game['away_team']}\n")
+            for b in game["bookmakers"]:
+                if b["key"] == "draftkings":
+                    for m in b["markets"]:
+                        f.write(f"  Market: {m['key']}\n")
+                        for o in m["outcomes"]:
+                            f.write(f"    {o['name']}: {o['point']}\n")
 
 
     for game in data:
